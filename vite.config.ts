f797d2277c39a9f3e32a218702c1288cc73080a5
Sophie Park <sophie.park@dali.dartmouth.edu>
2025-05-21 23:12:57 -0500

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './src'),
-      '@': path.resolve(__dirname, './src'),
+      '~': resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
   server: {
@@ -15,7 +19,19 @@ export default defineConfig({
       '/api/notion': {
         target: 'https://api.notion.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/notion/, ''),
+        rewrite: (path: string) => path.replace(/^\/api\/notion/, ''),
+        headers: {
+          'Notion-Version': '2022-02-22',
+        },
+      },
+    },
+  },
+  preview: {
+    proxy: {
+      '/api/notion': {
+        target: 'https://api.notion.com',
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api\/notion/, ''),
         headers: {
           'Notion-Version': '2022-02-22',
         },
